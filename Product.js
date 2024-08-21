cd3f05404326ee4195f9099bff1dc00ef9435116
MrTamAn <mrtaman20@gmail.com>
2024-08-21 19:42:54 +0700

    2108

---
+++ Product.js
@@ -0,0 +1,20 @@
+class Product{
+id;
+name;
+image;
+subImage;
+salePrice;
+realPrice;
+release;
+discount;
+constructor(id,name,image,subImage,salePrice,realPrice,release,discount) {
+    this.id = id;
+    this.name = name;
+    this.image = image;
+    this.subImage = subImage;
+    this.salePrice = salePrice;
+    this.realPrice = realPrice;
+    this.release = release;
+    this.discount = discount;
+}
+}
---
+++ Đăng nhập/Account.js
@@ -0,0 +1,16 @@
+class Account{
+    surName;
+    givenName;
+    phoneNumber;
+    gender;
+    accounEmail;
+    password;
+constructor(surName,givenName,phoneNumber,gender,accountEmail,password) {
+    this.surName = surName;
+    this.givenName = givenName;
+    this.phoneNumber = phoneNumber;
+    this.gender = gender;
+    this.accounEmail = accountEmail;
+    this.password = password;
+}
+}
